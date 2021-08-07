2a18e9b446f7dbb304a2ef977ec03065a23c1f4b
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-08-07 02:24:47 -0300

    comparable

---
+++ EjemploComparableComparator/src/ComparableComparator/Theatre.java
@@ -0,0 +1,57 @@
+package ComparableComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Theatre {
+
+    private final String theatreName;
+    public List<Seat> seats = new ArrayList<>();
+
+    public Theatre(String theatreName, int numRows, int seatsPerRow) {
+        this.theatreName = theatreName;
+
+        int lastRow = 'A' + (numRows -1);
+        for (char row = 'A'; row <= lastRow; row++) {
+            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public boolean reserveSeat(String seatNumber) {
+        int low = 0;
+        int high = seats.size()-1;
+
+        while (low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int comparison = midVal.getSeatNumber().compareTo(seatNumber);
+
+            if (comparison < 0) {
+                low = mid + 1;
+            } else if (comparison > 0) {
+                high = mid - 1;
+            } else {
+                return seats.get(mid).reserve();
+            }
+        }
+        System.out.println("There is no seat " + seatNumber);
+        return false;
+    }
+
+
+    // for testing
+    public void getSeats() {
+        for(Seat seat : seats) {
+            System.out.println(seat.getSeatNumber());
+        }
+    }
+
+}
